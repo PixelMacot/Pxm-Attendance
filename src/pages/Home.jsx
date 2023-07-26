ccc121871fd7507ab012fe291fdd1f69e2276104
syprogrammer <sonaly647@gmail.com>
2023-07-26 13:17:39 +0530

    fixing err of home page first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { doc, setDoc, getDoc, updateDoc, Timestamp } from "firebase/firestore";
 import { db } from '../firebase';
 import CalendarApp from '../components/CalendarApp';
@@ -8,56 +8,47 @@ import { AuthContext } from '../context/AuthContext'
 import { CalendarContext } from '../context/CalendarContext'
 
 const Home = () => {
-  const [logged, SetLogged] = useState(false)
-  let [useruid,setUserUid]=useState()
-  // const [attendance, setAttendance] = useState("dummy")
-  // const [markdate, setMarkDate] = useState()
-  const {currentUser,userData} = useContext(AuthContext)
-  const {getAttendanceData,markAttendance,markdate,attendance,markdatefunction} = useContext(CalendarContext)
- 
+  const { currentUser, userData } = useContext(AuthContext)
+  const { getAttendanceData, markAttendance, markdate, attendance, markdatefunction } = useContext(CalendarContext)
+
   useEffect(() => {
-    const unsub = ()=>{
+    const unsub = () => {
       if (currentUser) {
         // User is signed in
         SetLogged(true)
-        setUserUid(currentUser.uid)
         getAttendanceData(currentUser.uid)
 
       }
-     
+
     }
     return () => {
       unsub();
     };
   }, []);
+
   useEffect(() => {
     markdatefunction()
-  
-    return () => {
-   
-    }
+    return () => { }
   }, [attendance])
-  
+
 
   return (
     <section className='min-h-[100vh]'>
-      {
-        logged && (
-          <div className="">
-            <Profile userData={userData} markAttendance={markAttendance} show={true} datearr={markdate}/>
-            {/* //attendance mark */}
-          
-            {/* //user-attendance-info */}
-            <div className="user-attendance border w-[90%] mx-auto my-8 p-5 shadow-md flex items-center justify-center">
-              {
-                markdate && (
-                  <CalendarApp arr={markdate} user={userData}/>
-                )
-              }
-            </div>
-          </div>
-        )
-      }
+
+      <div className="">
+        <Profile userData={userData} markAttendance={markAttendance} show={true} datearr={markdate} />
+        {/* //attendance mark */}
+
+        {/* //user-attendance-info */}
+        {/* <div className="user-attendance border w-[90%] mx-auto my-8 p-5 shadow-md flex items-center justify-center">
+          {
+            markdate && (
+              <CalendarApp arr={markdate} user={userData} />
+            )
+          }
+        </div> */}
+      </div>
+
     </section>
   )
 }
