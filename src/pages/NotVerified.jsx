4477c7ca516ab227cbf7020c7dd36fb3c2ece51a
syprogrammer <sonaly647@gmail.com>
2023-07-26 16:08:57 +0530

    not verified page responsive fix

--- src/pages/NotVerified.jsx
+++ src/pages/NotVerified.jsx
@@ -5,10 +5,10 @@ const NotVerified = () => {
     <div className="">
       <div className="min-h-[70vh]">
         <div className="shadow-md p-2 ">
-          <img src="/logo.png" alt="" className='w-[40%]' />
+          <img src="/logo.png" alt="" className='w-[40%] md:w-[200px]' />
         </div>
         <div className="flex flex-col justify-center items-center my-20">
-          <img src="/warning.png" alt="" className='w-[40%]' />
+          <img src="/warning.png" alt="" className='w-[40%] md:w-[200px]' />
           <div>
             Verify Your Email proceed further
           </div>
