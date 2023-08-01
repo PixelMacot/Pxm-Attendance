649ac277ea3c5d4756d4381200527ee7feacbe22
syprogrammer <sonaly647@gmail.com>
2023-08-01 09:45:13 +0530

    crreated avatar circular

--- src/components/loader/HomePageLoader.jsx
+++ src/components/loader/HomePageLoader.jsx
@@ -5,8 +5,8 @@ const HomePageLoader = () => {
         <div className="loader flex flex-col justify-center items-center py-60">
             {/* <img src="/loadericon.svg" alt="" /> */}
             <img src="/logo.png" alt="" className='w-[50%]'/>
-            <div class="jumping-dots-loader"> <span></span> <span></span> <span></span> </div>
-            <div class="moving-gradient"></div>
+            <div className="jumping-dots-loader"> <span></span> <span></span> <span></span> </div>
+            <div className="moving-gradient"></div>
         </div>
     )
 }
